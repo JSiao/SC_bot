0266088898c0ff4d12c16567f1e6089d4e6419c6
JSiao <jecsiao@gmail.com>
2018-01-18 20:55:01 -0800

    updated bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 var Discord = require('discord.io');
 var logger  = require('winston');
 var auth    = require('./auth.json');
+//var SC      = require('node-soundcloud');
 
 logger.remove(logger.transports.Console);
 logger.add(logger.transports.Console, {
@@ -13,6 +14,15 @@ var bot = new Discord.Client(
     token: auth.token,
     autorun: true
 });
+
+/*
+SC.init({
+    id: '',
+    secret: '',
+    uri:
+});
+*/
+
 const token = process.env.TOKEN;
 
 bot.on('ready', function (evt)
@@ -32,13 +42,34 @@ bot.on('message', function (user, userID, channelID, message, evt)
         switch(cmd)
         {
             case 'hello':
-              bot.sendMessage(
-              {
-                  to: channelID,
-                  message: 'Yo!'
-              }
-              );
-              break;
+                bot.sendMessage(
+                {
+                    to: channelID,
+                    message: 'Yo!'
+                }
+                );
+                break;
+            case 'roll':
+                app_roll(args[1]);
+                break;
         }
     }
 });
+
+function app_roll(arg)
+{
+    var nums = arg.split(/d| /, 1);
+    if (nums[0] = "") nums[0] = 1;
+    var array[nums[0]];
+    var sum = 0;
+    for (int h = 0; h < nums[0]; h++)
+    {
+        sum += array[h] = Math.round(Math.random() * nums[1]);
+    }
+    bot.sendMessage(
+    {
+        to: channelID,
+        message: sum
+    }
+    );
+}
